e1b81c0cb7dd5e4822ab70acfc7b57896c130284
Henrik Ma Johansson <dahankzter@gmail.com>
2026-02-11 18:45:52 +0100

    fix: Update benchmark files to use rand::RngExt
    The rand 0.10 API changed, requiring RngExt trait to be in scope
    to use the random_range() method. This fixes CI build failures.
    
    Signed-off-by: Henrik Ma Johansson <dahankzter@gmail.com>

--- glommio/benches/competing_io.rs
+++ glommio/benches/competing_io.rs
@@ -5,7 +5,7 @@ use glommio::{
     io::{ImmutableFile, ImmutableFileBuilder},
     Latency, LocalExecutorBuilder, Placement, Shares,
 };
-use rand::Rng;
+use rand::RngExt;
 use std::{
     cell::{Cell, RefCell},
     path::PathBuf,
